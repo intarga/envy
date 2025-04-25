3335b90242a4d27755b7ed92c064094fb5681650
Ingrid Abraham <git@ingrids.email>
2025-04-25 19:15:01 +0200

    switch text_file from Rope to Vec<String>

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,9 @@ use itertools::{
     FoldWhile::{Continue, Done},
     Itertools,
 };
-use ropey::{Rope, RopeSlice};
 use std::{
     fs::File,
-    io::{self, BufReader, Stdout, Write, stdin, stdout},
+    io::{self, BufRead, BufReader, Stdout, Write, stdin, stdout},
     ops::{Add, Sub},
 };
 use termion::{
@@ -15,16 +14,18 @@ use termion::{
     screen::{AlternateScreen, IntoAlternateScreen},
 };
 
+// TODO: operate on graphemes not chars
+
 const SCROLLOFF: usize = 5;
 
 type Term = AlternateScreen<RawTerminal<Stdout>>;
 
 struct CursorPosition {
-    /// Ropey char_idx (char in file cursor is on)
+    line: usize,
     char: usize,
-    /// if a vertical move causes a change in cursor column (because the new line is too short)
+    /// If a vertical move causes a change in cursor column (because the new line is too short),
     /// this stores the original column so it can be restored on a subsequent vertical move
-    target_col: Option<u16>,
+    target_char: Option<usize>,
 }
 
 struct TermSize {
@@ -32,14 +33,14 @@ struct TermSize {
     rows: u16,
 }
 
-#[derive(Eq, Ord, PartialEq, PartialOrd)]
+#[derive(Debug, Clone, Eq, Ord, PartialEq, PartialOrd)]
 struct ViewOffset {
     line: usize,
     wrap: usize,
 }
 
 struct EditorState {
-    file_text: Rope,
+    file_text: Vec<String>,
     /// first line in file to draw
     view_offset: ViewOffset,
     cursor_position: CursorPosition,
@@ -50,14 +51,18 @@ impl EditorState {
     fn new(path: String) -> Self {
         let (cols, rows) = termion::terminal_size().unwrap();
 
-        let file_text = Rope::from_reader(BufReader::new(File::open(path).unwrap())).unwrap();
+        let file_text = BufReader::new(File::open(path).unwrap())
+            .lines()
+            .collect::<Result<Vec<String>, std::io::Error>>()
+            .unwrap();
 
         EditorState {
             file_text,
             view_offset: ViewOffset { line: 0, wrap: 0 },
             cursor_position: CursorPosition {
+                line: 0,
                 char: 0,
-                target_col: None,
+                target_char: None,
             },
             term_size: TermSize { cols, rows },
         }
@@ -66,29 +71,25 @@ impl EditorState {
     fn visual_lines_in_range(&self, range: std::ops::Range<usize>) -> usize {
         range
             .map(|line_idx| {
-                (self.file_text.line(line_idx).len_chars() / self.term_size.cols as usize) + 1
+                (self.file_text[line_idx].len().add(1) / self.term_size.cols as usize) + 1
             })
             .sum::<usize>()
     }
 
     fn get_cursor_visual_pos(&self) -> (u16, u16) {
-        let line = self.file_text.char_to_line(self.cursor_position.char);
-        let line_start = self.file_text.line_to_char(line);
-
-        let col = (((self.cursor_position.char - line_start) % self.term_size.cols as usize) + 1)
+        let col = ((self.cursor_position.char % self.term_size.cols as usize) + 1)
             .try_into()
             .unwrap();
 
         let visual_lines_above: u16 = self
-            .visual_lines_in_range(self.view_offset.line..line)
+            .visual_lines_in_range(self.view_offset.line..self.cursor_position.line)
             // TODO: should this be in visual_lines_in_range? or is it because terminal size is 1-based?
             .add(1)
             // account for the visual lines of the first line not shown
             .sub(self.view_offset.wrap)
             .try_into()
             .unwrap();
-        let breaks_in_curr_line =
-            (self.cursor_position.char - line_start) / self.term_size.cols as usize;
+        let breaks_in_curr_line = self.cursor_position.char / self.term_size.cols as usize;
         let row = visual_lines_above + breaks_in_curr_line as u16;
 
         (col, row)
@@ -96,23 +97,20 @@ impl EditorState {
 
     /// Number of term rows a line takes up when wrapped
     fn line_visual_height(&self, line_idx: usize) -> usize {
-        self.file_text.line(line_idx).len_chars() / self.term_size.cols as usize
+        self.file_text[line_idx].len() + 1 / self.term_size.cols as usize
     }
 
-    /// Returns the view offset `distance` term rows up from the char_idx represented by start
-    fn offset_at_visual_distance_up(&self, start: usize, distance: usize) -> ViewOffset {
-        let start_line = self.file_text.char_to_line(start);
-        let start_wrap =
-            (self.file_text.line_to_char(start_line) - start) / self.term_size.cols as usize;
+    /// Returns the view offset `distance` term rows up from the offset represented by start
+    fn offset_at_visual_distance_up(&self, start: ViewOffset, distance: usize) -> ViewOffset {
+        // let start_line = self.file_text.char_to_line(start);
+        // let start_wrap =
+        //     (self.file_text.line_to_char(start_line) - start) / self.term_size.cols as usize;
 
-        // TODO: this code assumes start is a line_idx, but it should take char_idx
-        // also, we need to account for wrapping in the current line, as that char_idx may not
-        // align with a line start
-        let (logical_line, visual_distance) = (start_line..0)
+        let (logical_line, visual_distance) = (start.line..0)
             // skip current line, we account for it by using start_wrap in the fold_while acc
             .skip(1)
             .map(|line_idx| self.line_visual_height(line_idx))
-            .fold_while((0, start_wrap), |(logi_acc, visu_acc), x| {
+            .fold_while((0, start.wrap), |(logi_acc, visu_acc), x| {
                 let logi_next = logi_acc + 1;
                 let visu_next = visu_acc + x;
                 if visu_next > distance {
@@ -135,10 +133,17 @@ impl EditorState {
 
     // TODO: better name?
     fn ensure_cursor_in_view(&mut self) {
+        let offset_at_cursor = ViewOffset {
+            line: self.cursor_position.line,
+            wrap: (self.file_text[self.cursor_position.line].len() + 1)
+                / self.term_size.cols as usize,
+        };
+
         // lower here means visually lower on the screen, not lower number
-        let lower_bound = self.offset_at_visual_distance_up(self.cursor_position.char, SCROLLOFF);
+        let lower_bound = self.offset_at_visual_distance_up(offset_at_cursor.clone(), SCROLLOFF);
+        // TODO: can be calculated less redundantly relative to lower_bound
         let upper_bound = self.offset_at_visual_distance_up(
-            self.cursor_position.char,
+            offset_at_cursor,
             self.term_size.rows as usize - SCROLLOFF,
         );
 
@@ -150,81 +155,64 @@ impl EditorState {
     }
 
     fn cursor_left(&mut self) {
-        self.cursor_position.char = self.cursor_position.char.saturating_sub(1);
-        self.cursor_position.target_col = None
+        if self.cursor_position.char > 0 {
+            self.cursor_position.char -= 1;
+        } else if self.cursor_position.line > 0 {
+            self.cursor_position.line -= 1;
+            self.cursor_position.char = self.file_text[self.cursor_position.line].len();
+        }
+        self.cursor_position.target_char = None
     }
 
     fn cursor_right(&mut self) {
-        self.cursor_position.char =
-            (self.cursor_position.char + 1).min(self.file_text.len_chars() - 1);
-        self.cursor_position.target_col = None
+        if self.cursor_position.char < self.file_text[self.cursor_position.line].len() {
+            self.cursor_position.char += 1;
+        } else if self.cursor_position.line < self.file_text.len() - 1 {
+            self.cursor_position.line += 1;
+            self.cursor_position.char = 0;
+        }
+        self.cursor_position.target_char = None
     }
 
     fn cursor_up(&mut self) {
-        let curr_line = self.file_text.char_to_line(self.cursor_position.char);
-        if curr_line < 1 {
+        if self.cursor_position.line < 1 {
             return;
         }
 
-        let start_of_curr_line = self.file_text.line_to_char(curr_line);
-        let start_of_prev_line = self.file_text.line_to_char(curr_line - 1);
-        let end_of_prev_line = start_of_curr_line - 1;
-
-        let curr_col: u16 = (self.cursor_position.char - start_of_curr_line)
-            .try_into()
-            .unwrap();
-        let target_col = self
+        let new_line = self.cursor_position.line - 1;
+        let target_char = self
             .cursor_position
-            .target_col
-            .map_or(curr_col, |target_col| target_col.max(curr_col));
-
-        let optimistic_next_pos = start_of_prev_line + target_col as usize;
-
-        self.cursor_position = if optimistic_next_pos > end_of_prev_line {
-            CursorPosition {
-                char: end_of_prev_line,
-                target_col: Some(target_col),
-            }
-        } else {
-            CursorPosition {
-                char: optimistic_next_pos,
-                target_col: None,
-            }
-        }
+            .target_char
+            .unwrap_or(self.cursor_position.char);
+        let new_line_end = self.file_text[new_line].len();
+        (self.cursor_position.char, self.cursor_position.target_char) =
+            if new_line_end < target_char {
+                (new_line_end, Some(target_char))
+            } else {
+                (target_char, None)
+            };
+        self.cursor_position.line = new_line;
     }
 
-    // TODO: DRY potential with cursor_up
     fn cursor_down(&mut self) {
-        let curr_line = self.file_text.char_to_line(self.cursor_position.char);
-        if !(curr_line < self.file_text.len_lines()) {
+        if self.cursor_position.line >= self.file_text.len() - 1 {
+            // TODO handle invalid cursor?
             return;
         }
 
-        let start_of_curr_line = self.file_text.line_to_char(curr_line);
-        let start_of_next_line = self.file_text.line_to_char(curr_line + 1);
-        let end_of_next_line = self.file_text.line_to_char(curr_line + 2) - 1;
-
-        let curr_col: u16 = (self.cursor_position.char - start_of_curr_line)
-            .try_into()
-            .unwrap();
-        let target_col = self
+        let new_line = self.cursor_position.line + 1;
+        let target_char = self
             .cursor_position
-            .target_col
-            .map_or(curr_col, |target_col| target_col.max(curr_col));
-
-        let optimistic_next_pos = start_of_next_line + target_col as usize;
-
-        self.cursor_position = if optimistic_next_pos > end_of_next_line {
-            CursorPosition {
-                char: end_of_next_line,
-                target_col: Some(target_col),
-            }
-        } else {
-            CursorPosition {
-                char: optimistic_next_pos,
-                target_col: None,
-            }
-        }
+            .target_char
+            .unwrap_or(self.cursor_position.char);
+        let new_line_end = self.file_text[new_line].len();
+        (self.cursor_position.char, self.cursor_position.target_char) =
+            if new_line_end < target_char {
+                (new_line_end, Some(target_char))
+            } else {
+                (target_char, None)
+            };
+        self.cursor_position.line = new_line;
     }
 }
 
@@ -270,33 +258,39 @@ fn process_keypress(key: Key, state: &mut EditorState) -> bool {
         _ => (),
     }
 
-    return false;
+    false
 }
 
 // TODO: more descriptive name?
-fn layout(state: &EditorState) -> Vec<RopeSlice<'_>> {
+fn layout(state: &EditorState) -> Vec<&str> {
     let width = state.term_size.cols as usize;
     let mut visual_lines = Vec::with_capacity(state.term_size.rows.into());
     let mut push_count = 0;
 
-    'outer: for line in state.file_text.lines().skip(state.view_offset.line) {
-        let line = line.slice(..(line.len_chars().saturating_sub(1)));
-        let wrap_count = (line.len_chars() / width) + 1;
+    for line in state.file_text.iter().skip(state.view_offset.line) {
+        if line.len() < width {
+            visual_lines.push(line.as_str());
+            push_count += 1
+        } else {
+            // let wrap_count = ((line.len() + 1) / width) + 1;
 
-        // wrap at multiples of the viewport width, with 0 and line length as outer bounds
-        let breakpoints = (0..wrap_count)
-            .map(|i| i * width)
-            .chain(std::iter::once(line.len_chars()))
-            .collect::<Vec<usize>>();
-
-        // take breakpoints in pairs, since we slice from one point to the next
-        for points in breakpoints.windows(2) {
-            visual_lines.push(line.slice(points[0]..points[1]));
-            push_count += 1;
+            let mut rest = line.as_str();
+            let mut char_indices = line.char_indices();
+            while let Some((i, _)) = char_indices.nth(width) {
+                let (p1, p2) = rest.split_at(i);
+                visual_lines.push(p1);
+                push_count += 1;
+                rest = p2;
+            }
+            if !rest.is_empty() {
+                visual_lines.push(rest);
+                push_count += 1;
+            }
         }
+
         if push_count - state.view_offset.wrap > state.term_size.rows.into() {
             // we have enough visual lines to fill the viewport
-            break 'outer;
+            break;
         }
     }
 
@@ -305,12 +299,21 @@ fn layout(state: &EditorState) -> Vec<RopeSlice<'_>> {
 
 #[test]
 fn test_layout() {
+    // let file_text = BufReader::new(File::open(path).unwrap())
+    //     .lines()
+    //     .collect::<Result<Vec<String>, std::io::Error>>()
+    //     .unwrap();
     let state = EditorState {
-        file_text: Rope::from_str("\n\n1234567\n1234\n\n"),
+        // file_text: Rope::from_str("\n\n1234567\n1234\n\n"),
+        file_text: "\n\n1234567\n1234\n\n"
+            .lines()
+            .map(|l| l.to_string())
+            .collect(),
         view_offset: ViewOffset { line: 2, wrap: 0 },
         cursor_position: CursorPosition {
-            char: 3,
-            target_col: None,
+            line: 2,
+            char: 1,
+            target_char: None,
         },
         term_size: TermSize { cols: 3, rows: 5 },
     };
@@ -351,7 +354,7 @@ fn main() {
     let stdin = stdin();
     let mut term = init_tui().unwrap();
 
-    let path = std::env::args().skip(1).next().unwrap();
+    let path = std::env::args().nth(1).unwrap();
 
     let mut state = EditorState::new(path);
 
